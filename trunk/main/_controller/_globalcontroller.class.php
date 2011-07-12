d5f61ef500e8aae49b3d54eb64c0dadeb82b7406
oskerker <oskerker@3f40c12c-4e58-023f-1954-704e2b174585>
2011-07-12 07:54:10 +0000

    -

--- trunk/main/_controller/_globalcontroller.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class GlobalController extends Controller {
-	public function setup() {
-		$this->view->setHandler('serpent');
-	}
-}
--- trunk/main/_i18n/de/_tree.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-### Anmelden einer Seite und Vergabe des Titels
-$tree['meta']['home']				= 'Startseite';
-
-$tree['hidden']['home']		= 'Startseite_hidden';
-$tree['hidden']['home_kontakt']		= 'Kontakt_hidden';
-$tree['hidden']['home_kontakt_form']		= 'Form_hidden';
