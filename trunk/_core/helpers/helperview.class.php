d318f7510661c048d11fef36ea121e4a120a0ec5
erdmann.tester@gmail.com <erdmann.tester@gmail.com@3f40c12c-4e58-023f-1954-704e2b174585>
2011-08-17 07:59:42 +0000

    added hidelink() in helperView

--- trunk/_core/helpers/helperview.class.php
+++ trunk/_core/helpers/helperview.class.php
@@ -57,6 +57,16 @@ class HelperView
 		return $returner;
 		}
 
+	public static function hidelink($url, $text = '', $html = '')
+		{
+		if (empty($text)) $text = $url;
+		$link = '<a href="'.$url.'" '.$html.' rel="nofollow">'.htmlspecialchars($text).'</a>';
+		$returner = '<script type="text/javascript">/* <![CDATA[ */'."\n";
+		$returner .= 'document.write("'.str_replace('"', '\"', strrev($link) ).'".split("").reverse().join("").replace(/--/g, "@"));'."\n";
+		$returner .= '/* ]]> */</script>';
+		return $returner;
+		}
+
 	public static function loremipsum($word_count = 200, $random = true)
 		{
 		$text = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.';
